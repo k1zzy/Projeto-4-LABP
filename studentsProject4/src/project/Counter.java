b74ace89afedb6ae5ad933b5c62faae09916c001
rodrigo <rodrigo.bb@outlook.pt>
2024-05-03 02:32:14 +0100

    esta feito meu amigo javadocs e passa a tudo bora la!!!

--- studentsProject4/src/project/Counter.java
+++ studentsProject4/src/project/Counter.java
@@ -1,5 +1,14 @@
 package project;
 
+/**
+ * Classe que representa um Counter de uma loja.
+ * Um Counter é um balcão de atendimento que processa clientes.
+ * Cada balcão tem um id, um tempo atual, um montante total de vendas, um tempo total de processamento,
+ * 
+ * @author Rodrigo Afonso (61839)
+ * @version 1.0
+ */
+
 import java.util.Locale;
 
 public class Counter {
@@ -11,6 +20,14 @@ public class Counter {
 	private StringBuilder log;
 	public static final String EOL = System.lineSeparator();
 	
+	/** 
+	 * Contrutor da classe Counter que representa um balcão que recebe
+	 * um id, o tempo atual e um StringBuilder para o log.
+	 * 
+	 * @param counterId id do balcão
+	 * @param currentTime tempo atual
+	 * @param sb StringBuilder para o log
+	 */
 	public Counter(int counterId, int currentTime, StringBuilder sb) {
 		this.counterId = counterId;
 		this.currentTime = currentTime;
@@ -24,6 +41,14 @@ public class Counter {
         log.append(EOL);
 	}
 	
+	
+	/** 
+	 * Adiciona um cliente à frente da fila do balcão.
+	 * Adiciona o tempo de processamento do cliente ao tempo total de processamento do balcão.
+	 * Processa a fila de clientes até ao tempo de chegada do cliente.
+	 * 
+	 * @param client cliente a adicionar
+	 */
 	public void addClient(Client client) {
 	    totalProcessingDuration += client.getClientProcessingDuration();
 	    processQueueUntilTime(client.getArrivalTime());
@@ -31,7 +56,7 @@ public class Counter {
 	}
 	
 	/**
-	 * Devolve o cliente que está a ser processado.
+	 * Devolve o cliente que está à frente na fila (que está a ser processado).
 	 * 
 	 * @requires A fila de clientes não estar vazia
 	 * @return o cliente que está à frente na fila
@@ -40,15 +65,33 @@ public class Counter {
 		return clientQueue.front();
 	}
 	
+	/**
+	 * Remove o cliente que está à frente na fila (se já foi processado totalmente).
+	 * Retira do tempo total de processamento do balcão o tempo de processamento do cliente.
+	 */
 	public void removeClient() {
 	    totalProcessingDuration -= currentClient().getClientProcessingDuration();
 		clientQueue.dequeue();
 	}
 	
+	
+	/** 
+	 * Verifica se a fila de clientes está vazia.
+	 * 
+	 * @return boolean true se a fila estiver vazia, false caso contrário
+	 */
 	public boolean isEmpty() {
 	    return clientQueue.isEmpty();
 	}
 	
+	
+	/** 
+	 * Processa a fila de clientes durante um determinado tempo.
+	 * Caso o tempo de processamento do cliente seja menor ou igual ao tempo de processamento,
+	 * o cliente é processado e removido da fila.
+	 * 
+	 * @param duration tempo de processamento
+	 */
 	public void processQueueForDuration(int duration) {
 	    // enquanto existir cliente e enquanto for possivel processa-lo
 	    while (!isEmpty() && duration >= currentClient().getClientProcessingDuration()) {
@@ -71,7 +114,17 @@ public class Counter {
 	    }
 	}
 	
+	
+	/** 
+	 * Processa a fila de clientes até um determinado momento no tempo.
+	 * Caso o tempo de processamento do cliente seja menor ou igual ao tempo de processamento,
+	 * o cliente é processado e removido da fila.
+	 *
+	 * @param time tempo até ao qual se pretende processar a fila
+	 */
 	public void processQueueUntilTime(int time) {
+		// funciona exatamente igual ao processQueueForDuration
+		// assim que ao time se subtrai o currentTime
 	    time -= currentTime;
         while (!isEmpty() && time >= currentClient().getClientProcessingDuration()) {
             time -= currentClient().getClientProcessingDuration();
@@ -91,6 +144,13 @@ public class Counter {
         }
 	}
 	
+	
+	/** 
+	 * Devolve o preço total de compras num dado carrinho de compras. 
+	 * 
+	 * @param shoppingCart array de produtos
+	 * @return total de compras do cliente
+	 */
 	private static double getClientSalesAmount(Product[] shoppingCart) {
         double total = 0;
         for (Product p : shoppingCart) {
@@ -99,18 +159,42 @@ public class Counter {
         return total;
     }
 	
+	
+	/** 
+	 * Devolve o id do balcão.
+	 * 
+	 * @return id do balcão
+	 */
 	public int getCounterId() {
 	    return counterId;
 	}
 	
+	
+	/** 
+	 * Devolve o tempo atual.
+	 * 
+	 * @return tempo atual
+	 */
 	public int getCurrentTime() {
 	    return currentTime;
 	}
 	
+	
+	/**
+	 * Devolve o montante total de vendas. 
+	 * 
+	 * @return o montante total de vendas
+	 */
 	public double getSalesAmount() {
 	    return salesAmount;
 	}
 	
+	
+	/** 
+	 * Devolve o tempo total de processamento do balcão.
+	 * 
+	 * @return tempo total de processamento do balcão
+	 */
 	public int getTotalProcessingDuration() {
 	    return totalProcessingDuration;
 	}
--- studentsProject4/src/project/Store.java
+++ studentsProject4/src/project/Store.java
@@ -1,5 +1,13 @@
 package project;
 
+/**
+ * Classe que representa uma Store com um stock de produtos e um número de balcões.
+ * Uma Store é uma loja que processa clientes e vende produtos.
+ * 
+ * @author Rodrigo Afonso (61839)
+ * @version 1.0
+ */
+
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Arrays;
@@ -13,19 +21,49 @@ public class Store {
     private StringBuilder sb;
     public static final String EOL = System.lineSeparator();
     
+    /** 
+     * Construtor da classe Store que representa uma loja que recebe
+     * um ficheiro para ler o stock e a quantidade de balcões e um StringBuilder que representa o log.
+     * 
+     * @throws IllegalArgumentException se o ficheiro não existir ou se o formato do ficheiro for inválido
+     * @param fileName ficheiro com o stock
+     * @param sb StringBuilder para o log
+     */
     public Store(String fileName, StringBuilder sb) {
         this.sb = sb;
-        String[] catalog = readStore(fileName); // le o ficheiro e guarda o conteudo num array
-        this.counters = new Counter[Integer.parseInt(catalog[0])]; // cria os balcoes com o tamanho indicado
-        this.productCatalog = new Product[Integer.parseInt(catalog[1])]; // cria o catalogo de produtos com o tamanho indicado
+        String[] catalog;
+        try {
+           catalog = readStore(fileName); // le o ficheiro e guarda o conteudo num array
+        }
+        catch (IllegalArgumentException e) {
+            throw new IllegalArgumentException(e.getMessage());
+        }
+        try {
+            this.counters = new Counter[Integer.parseInt(catalog[0])]; // cria os balcoes com o tamanho indicado
+            this.productCatalog = new Product[Integer.parseInt(catalog[1])]; // cria o catalogo de produtos com o tamanho indicado
+        }
+        catch (NumberFormatException e) {
+            throw new IllegalArgumentException("Invalid stock file format");
+        }
         fillProducts(catalog); // preenche o catalogo com os produtos do ficheiro
         iniciateCounters(); // inicia os balcoes
     }
     
+    
+    /** 
+     * Lê o ficheiro com o stock e o número de balcões a abrir e
+     * devolve um array de strings com o conteúdo.
+     * 
+     * @param fileName nome do ficheiro
+     * @throws IllegalArgumentException se o ficheiro não existir
+     * @return String[] array de strings com o conteúdo do ficheiro
+     */
     private static String[] readStore(String fileName) {
         StringBuilder sb = new StringBuilder();
         try {
             Scanner sc = new Scanner(new File(fileName));
+            // le o ficheiro e guarda o conteudo num StringBuilder
+            // enquanto houver linhas no ficheiro
             while (sc.hasNextLine()) {
                 sb.append(sc.nextLine());
                 sb.append(" ");
@@ -33,14 +71,23 @@ public class Store {
             sc.close();
         } 
         catch (FileNotFoundException e) {
-            e.printStackTrace(); //TODO esta estranho
+            throw new IllegalArgumentException("Stock file does not exist");
         }
+        // divide o conteudo do ficheiro por espaços e devolve-o num array
         return sb.toString().split(" ");
     }
     
+    
+    /** 
+     * Preenche o catalogo de produtos com os produtos do ficheiro de stock.
+     * 
+     * @param catalog array de strings com o conteúdo do ficheiro
+     */
     private void fillProducts(String[] catalog) {
         try {
             for (int i = 0; i < productCatalog.length; i++) {
+                // como coloquei o conteudo do ficheiro num array de strings todos de seguida
+                // o indice do array que pretendo é:
                 // i*2+2+i (funciona cheguei la por tentativa e erro)
                 productCatalog[i] = new Product(catalog[i*2+2+i], Double.parseDouble(catalog[i*2+3+i]), Integer.parseInt(catalog[i*2+4+i]));
             }
@@ -51,23 +98,37 @@ public class Store {
         }
     }
     
+    /** 
+     * Inicia o número de balcões indicado no ficheiro de stock com os parametros iniciais.
+     */
     private void iniciateCounters() {
         for (int i = 0; i < counters.length; i++) {
             counters[i] = new Counter(i, 0, this.sb);
         }
     }
     
+    
+    /** 
+     * Lê o ficheiro de eventos e processa-os, atribuindo os clientes aos balcões que terminam mais cedo,
+     * ou abrindo balcões se solicitado.
+     * No final, processa todos os balcões até que todos os clientes tenham sido processados,
+     * calcula o total de vendas e escreve tudo no log.
+     * 
+     * @param fileName nome do ficheiro de eventos
+     * @throws FileNotFoundException se o ficheiro de eventos não existir
+     */
     public void processEvents(String fileName) throws FileNotFoundException {
-        int currentTime = 0;
-        int currentUserId = 0;
-        int currentLine = 0;
-        int clientProcessingDuration = 0;
-        Scanner sc = null;
+        int currentTime = 0; // tempo atual
+        int currentUserId = 0; // id do cliente atual
+        int currentLine = 0; // linha atual do ficheiro
+        int clientProcessingDuration = 0; // tempo de processamento do cliente
+        Scanner sc = null; // scanner para ler o ficheiro
         try {
+            // tenta abrir o ficheiro
             sc = new Scanner(new File(fileName));
         }
         catch (FileNotFoundException e) {
-            sc.close();
+            //sc.close();
             throw new FileNotFoundException("Events file does not exist");
         }
         
@@ -79,6 +140,7 @@ public class Store {
             if (event[1].equals("CLIENT")) { // se o evento for do tipo CLIENT
                 Product[] cart = new Product[event.length - 3];
                 try {
+                    // tenta converter os parametros do evento para inteiros
                     currentTime = Integer.parseInt(event[0]);
                     currentUserId = Integer.parseInt(event[2]);
                 }
@@ -107,6 +169,7 @@ public class Store {
                     sc.close();
                     throw new IllegalArgumentException("Invalid event format in line " + currentLine);
                 }
+                // cria um novo balcao e processa todos os balcoes ate ao tempo atual
                 counters = Arrays.copyOf(counters, counters.length+1); // extende a array
                 counters[counters.length-1] = new Counter(counters.length-1, currentTime, this.sb);
                 processAllCountersUntil(currentTime);
@@ -120,13 +183,15 @@ public class Store {
         sb.append("All clients have been assigned to a counter!");
         sb.append(EOL);
         
-        while (!areAllCountersDone()) {
+        // processa todos os balcoes ate que todos os clientes tenham sido processados
+        while (!isProcessingFinished()) {
             processAllCountersFor(counters[firstCounterToFinishClient()].currentClient().getClientProcessingDuration());
         }
         
         sb.append("All clients have been processed!");
         sb.append(EOL);
         
+        // calcula o total de vendas
         for (Counter c : counters) {
             totalSalesAmount += c.getSalesAmount();
         }
@@ -135,28 +200,37 @@ public class Store {
         
         sc.close();
     }
-    
-    private boolean areAllCountersDone() {
-        for (Counter c : counters) {
-            if (c.getTotalProcessingDuration() > 0) {
-                return false;
-            }
-        }
-        return true;
-    }
-    
+   
+    /** 
+     * Processa todos os balcoes até um determinado momento no tempo.
+     * 
+     * @param untilTime tempo até ao qual se pretende processar os balcoes
+     */
     private void processAllCountersUntil(int untilTime) {
         for (Counter c : counters) {
             c.processQueueUntilTime(untilTime);
         }
     }
     
+    
+    /**
+     * Processa todos os balcões durante um determinado periodo de tempo.
+     * 
+     * @param tillTime tempo de processamento
+     */
     private void processAllCountersFor(int tillTime) {
         for (Counter c : counters) {
             c.processQueueForDuration(tillTime);
         }
     }
     
+    
+    /**
+     * Processa um cliente, atribuindo-o ao balcão que termina mais cedo.
+     * 
+     * @param client cliente a processar
+     * @return tempo atual após o processamento do cliente
+     */
     public int processClient(Client client) {
         int firstCounterToFinish = firstCounterToFinish();
         counters[firstCounterToFinish].addClient(client);
@@ -169,6 +243,12 @@ public class Store {
         return counters[firstCounterToFinish].getCurrentTime();
     }
     
+    
+    /**
+     * Devolve o id do balcão que termina mais cedo de processar todos os seus clientes.
+     * 
+     * @return o id do balcão que termina mais cedo
+     */
     public int firstCounterToFinish() {
         int first = 0;
         for (int i = 1; i < counters.length; i++) {
@@ -184,31 +264,54 @@ public class Store {
         return first;
     }
     
+    
+    /**
+     * Devolve o id do balcão que termina mais cedo de processar o seu cliente atual.
+     * 
+     * @return o id do balcão que termina mais cedo de processar o seu cliente atual
+     */
     public int firstCounterToFinishClient() {
         int first = 0;
         int firstTime = 0;
         int iTime = 0;
         for (int i = 1; i < counters.length; i++) {
+            // se o balcao first nao estiver vazio
             if (!counters[first].isEmpty()) {
+                // tempo do balcao first + tempo de processamento do cliente
                 firstTime = counters[first].getCurrentTime() + counters[first].currentClient().getClientProcessingDuration();
             }
+            // se estiver
             else {
+                // escolhe o balcao i como o primeiro
                 first = i;
                 continue;
             }
+            // se o balcao i nao estiver vazio
             if (!counters[i].isEmpty()) {
+                // tempo do balcao i + tempo de processamento do cliente
                 iTime = counters[i].getCurrentTime() + counters[i].currentClient().getClientProcessingDuration();
             }
+            // se estiver
             else {
+                // continua porque o first ja esta definido como first
                 continue;
             }
+            // se existir clientes em ambos os balcoes
+            // e o tempo do balcao i for menor que o tempo do balcao first
             if (iTime < firstTime) {
+                // balcao i passa a ser o first
                 first = i;
             }
         }
         return first;
     }
     
+    
+    /**
+     * Verifica se todos os clientes foram processados.
+     * 
+     * @return true se todos os clientes foram processados, false caso contrário
+     */
     public boolean isProcessingFinished() {
         for (Counter c : counters) {
             if (!c.isEmpty()) {
@@ -218,6 +321,12 @@ public class Store {
         return true;
     }
     
+    
+    /** 
+     * Devolve o total de vendas.
+     * 
+     * @return o total de vendas
+     */
     public double getTotalSalesAmount() {
         return totalSalesAmount;
     }
